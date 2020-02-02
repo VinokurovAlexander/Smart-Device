1aaace2dc469199cffa780b37b5b2a2b82a73932
Alexander <vinokurov.ag@mail.ru>
2020-02-02 20:33:22 +0300

    Адаптировал footer под мобильное разрешение

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,8 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+//accordion
+$('.accordion__btn').on('click', function() {
+  const parent = $(this).closest('.accordion');
+  parent.toggleClass('accordion--open');
+  parent.find('.accordion__item').slideToggle();
 });
