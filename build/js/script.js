726aaf3fab948342842e32f0130c355b6eb5e5b9
Alexander <vinokurov.ag@mail.ru>
2020-02-05 21:09:07 +0300

    Добавил фокус на поле с именем при окрытии формы и работу с localStorage

--- build/js/script.js
+++ build/js/script.js
@@ -7,11 +7,51 @@ $('.accordion__btn').on('click', function() {
   parent.find('.accordion__item').slideToggle();
 });
 
-//popup
+//popup init
 $('a[href="#callback-modal"]').magnificPopup({
   type: 'inline',
-  midClick: true
+  midClick: true,
+  focus: '#callback-user-name'
 });
+
+//localstorage
+const form = document.querySelector('#callback-modal');
+
+const formInput = {
+  userName: form.querySelector('#callback-user-name'),
+  phoneNumber: form.querySelector('#callback-phone-number'),
+  comment: form.querySelector('#callback-feedback-text')
+};
+
+let isStorageSupport = true;
+const storage = {
+  userName: '',
+  phoneNumber: '',
+  comment: ''
+};
+
+try {
+  Object.keys(storage).forEach((storageItemName) => {
+    storage[storageItemName] = localStorage.getItem(storageItemName);
+  })
+} catch (err) {
+  isStorageSupport = false;
+}
+
+Object.keys(storage).forEach((storageItemName) => {
+  if (storage[storageItemName]) {
+    formInput[storageItemName].value = storage[storageItemName];
+  }
+});
+
+form.addEventListener('submit', () => {
+  if (isStorageSupport) {
+    Object.keys(storage).forEach((storageItemName) => {
+      localStorage.setItem(storageItemName, formInput[storageItemName].value)
+    })
+  }
+});
+
 
 //input mask
 const phonesInput = $('input[name="phone-number"]');
