ef2d0860fcf5d116de79bc1d99b5319e2dc522aa
Alexander <vinokurov.ag@mail.ru>
2020-02-06 17:30:36 +0300

    Поправил работу аккардиона

--- build/js/app.js
+++ build/js/app.js
@@ -2,6 +2,7 @@
 
 $('.accordion__btn').on('click', function () {
   var parent = $(this).closest('.accordion');
+  $('.accordion--open').not(parent).toggleClass('accordion--open').find('.accordion__item').slideUp();
   parent.toggleClass('accordion--open');
   parent.find('.accordion__item').slideToggle();
 });
--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,5 +1,10 @@
 $('.accordion__btn').on('click', function() {
   const parent = $(this).closest('.accordion');
+
+  $('.accordion--open').not(parent)
+    .toggleClass('accordion--open')
+    .find('.accordion__item').slideUp();
+
   parent.toggleClass('accordion--open');
   parent.find('.accordion__item').slideToggle();
 });
